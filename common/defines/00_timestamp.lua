5d4adf5b89c30ba6c0142167d43d7d128ad29e04
MaxOttoFonShtirlits <shamarinna117@gmail.com>
2024-05-17 16:58:11 -0300

    Интеграция полезных маленьких модов в мод Часть 2 (Попытка номер 3)

---
+++ common/defines/00_timestamp.lua
@@ -0,0 +1,21 @@
+
+-- Spot Optimizations--------------------------------------------------------------------------------------
+NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 100
+NDefines.NGame.LAG_DAYS_FOR_PAUSE = 60
+NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 36
+NDefines.NGame.GAME_SPEED_SECONDS = { 0.35, 0.15, 0.1, 0.035, 0.0 }
+NDefines.NFocus.MAX_SAVED_FOCUS_PROGRESS = 30
+NDefines.NAI.EQUIPMENT_MARKET_BASE_MARKET_RATIO = 0.35
+
+Nlua = {
+
+NTopbar = {
+	GAME_SPEED_LIMIT = 0,	-- Unlocks Speed to match as much as the proccessor can handle
+	GAME_SPEED_STEPS = 5,	-- DONT CHANGE -- Deals with graphics and speed settings
+	GAME_SPEED_ONE = 1,	-- DONT CHANGE --
+	GAME_SPEED_TWO = 2,	-- DONT CHANGE --
+	GAME_SPEED_THREE = 3,	-- DONT CHANGE --
+	GAME_SPEED_FOUR = 4,	-- DONT CHANGE --
+	GAME_SPEED_FIVE = 5,	-- DONT CHANGE --
+	}
+}
